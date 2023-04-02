32eb799d760257fa85bbda5a2de72efb1dfeab1d
jasonwynn10 <jasonwynn10@me.com>
2023-04-02 08:35:26 -0400

    Fixed PathResult::getNextPosition() return type

--- src/salmonde/pathfinding/PathResult.php
+++ src/salmonde/pathfinding/PathResult.php
@@ -3,11 +3,12 @@ declare(strict_types = 1);
 
 namespace salmonde\pathfinding;
 
+use salmonde\pathfinding\astar\Node;
 use SplQueue;
 
 class PathResult extends SplQueue {
 
-	public function getNextPosition(): Vector3{
+	public function getNextPosition(): Node{
 		return $this->dequeue();
 	}
 }
